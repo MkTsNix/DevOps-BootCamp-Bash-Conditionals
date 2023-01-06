562d984720141d02f1cada43c24a61ce09493fc0
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-06 13:55:50 +0600

    Update check_string.sh

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Place your code here
+
+#!/bin/bash
+
+for i in $(echo "$1" | grep -o .)
+    do
+        case "$i" in
+            [\!\@\#\$\%\^\&\(\)\_\+\*] ) 
+	            ((symbols++));;
+            [0-9] )
+	            ((numbers++));;
+            [a-z-A-Z] )
+	            ((letters++));;
+        esac
+    done
+
+echo "Numbers: $numbers Symbols: $symbols Letters: $letters"
