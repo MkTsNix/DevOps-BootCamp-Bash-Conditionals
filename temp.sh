121cb1c04d63e153e5d212b795a1767337027276
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-06 13:54:39 +0600

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 # Place your code here
+
+temp_type=${1: -1}
+temperature=${1::-1}
+
+if [[ $temp_type == "K" ]]
+then
+    result=$(($temperature-273))
+    echo -n "$result"
+    echo "C"
+elif [[ $temp_type == "C" ]]
+then
+    result=$(($temperature+273))
+    echo -n "$result"
+    echo "K"
+fi
